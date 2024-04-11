05be856dd4bb78c451405ebb54a500e461b1bd5f
Teodor Haret <Teodor.Haret@amdocs.com>
2024-04-11 18:12:45 +0300

    Integration test - @SpringBootTest, @ActiveProfiles, @DirtiesContext, @Autowired, @EmbeddedKafka; awaitility, application-test.properties, spring.embedded.kafka.brokers

---
+++ src/test/java/integration/OrderDispatchIntegrationTest.java
@@ -0,0 +1,109 @@
+package integration;
+
+import dev.lydtech.dispatch.DispatchConfiguration;
+import dev.lydtech.dispatch.message.DispatchPreparing;
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.util.TestEventData;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+@SpringBootTest(classes = {DispatchConfiguration.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+@EmbeddedKafka(controlledShutdown = true)
+public class OrderDispatchIntegrationTest {
+    private final static String ORDER_CREATED_TOPIC = "order.created";
+
+    private final static String ORDER_DISPATHCED_TOPIC = "order.dispatched";
+
+    private final static String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+
+    @Autowired
+    KafkaTemplate kafkaTemplate;
+
+    @Autowired
+    private KafkaTestListener testListener;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry registry;
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public KafkaTestListener testListener() {
+            return new KafkaTestListener();
+        }
+    }
+
+    public static class KafkaTestListener {
+
+        AtomicInteger dispathPreparingCount = new AtomicInteger(0);
+        AtomicInteger orderDispatchedCounter = new AtomicInteger(0);
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = DISPATCH_TRACKING_TOPIC)
+        void receiveDispatchPreparing(@Payload DispatchPreparing payload) {
+            log.debug("Received DispatchPreparing: " +  payload);
+            dispathPreparingCount.incrementAndGet();
+
+        }
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+        testListener.dispathPreparingCount.set(0);
+        testListener.orderDispatchedCounter.set(0);
+
+        // instead of hard-coding delay, wire the registry bean from where we can get the listener container beans
+        //  and wait until the embedded broker ( also wired in ) has had its excepted partitions assigned
+
+        registry.getListenerContainers().stream().forEach(container ->
+                ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic()));
+    }
+
+    @Test
+    void testOrderDispathFlow() throws Exception {
+        OrderCreated orderCreated = TestEventData.buildOrderCreatedEvent(UUID.randomUUID(), "one-item");
+        sendMessage(ORDER_CREATED_TOPIC, orderCreated);
+
+        await().atMost(3, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
+                .until(testListener.dispathPreparingCount::get, equalTo(1));
+
+        await().atMost(1, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
+                .until(testListener.dispathPreparingCount::get, equalTo(1));
+    }
+
+    private void sendMessage(String topic, Object data) throws Exception {
+        kafkaTemplate.send(MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build()).get();
+    }
+}
